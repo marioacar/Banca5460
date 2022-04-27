f9cddb154e5ebbe880b8df17b0eda9e7940b1897
marioacar <mcarvantes@ucol.mx>
2022-04-27 12:37:38 -0500

    Agregar archivos de proyecto.

---
+++ Banca5460/Banca5460/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Banca5460.View5460;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Banca5460
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Principal5460());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Banca5460/Banca5460/Model5460/CuentaU5460.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banca5460.Model5460
+{
+    internal class CuentaU5460
+    {
+    }
+}
